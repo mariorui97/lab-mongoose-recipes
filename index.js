7d1f15974fcc266383ad5c13123ad2d126151027
Mário Rui <marinhocosta@live.com.pt>
2021-11-11 15:59:01 +0000

    week 4 lab day 4

--- index.js
+++ index.js
@@ -20,7 +20,28 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    let newRecipe = {title: 'Francesinha', 
+                     level: 'Easy Peasy',
+                     cuisine: 'Tuga',
+                     image: 'https://www.pingodoce.pt/wp-content/uploads/2017/09/francesinha.jpg'}
+
+    return Recipe.create(newRecipe)
+  })
+  .then(()=>{   
+    return Recipe.insertMany(data)
+  })
+  .then(()=>{
+    let query = {title: 'Rigatoni alla Genovese' };
+    return Recipe.findOneAndUpdate(query, { $set: {duration: 100}}, {useFindAndModify: false})
+  })
+  .then(()=>{   
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then((result)=>{   
+    console.log('Recipe deleted')
+  })
+  .then(()=>{   
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
